985f00cf4d4f76ec609a067138d0cdfaa1006757
IgorFelipe10 <113717418+IgorFelipe10@users.noreply.github.com>
2024-04-03 22:38:53 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,27 +37,15 @@ android {
 
 dependencies {
 
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("androidx.recyclerview:recyclerview:1.2.1")
-    implementation ("com.google.code.gson:gson:2.8.6")
-
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
-    implementation("androidx.compose.material3:material3-android:1.2.1")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-
     // Retrofit para requisiÃ§Ãµes HTTP e Gson para parse de JSON
-
-    // Retrofit para requisições HTTP e Gson para parse de JSON
-
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
@@ -67,8 +55,14 @@ dependencies {
     // Gson
     implementation ("com.google.code.gson:gson:2.8.6")
 
-    //Interceptor
+    //Logging
     implementation ("com.squareup.okhttp3:logging-interceptor:4.9.1")
+
+    //OkHTTP
     implementation ("com.squareup.okhttp3:okhttp:4.9.0")
+
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 
 }
