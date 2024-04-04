9621952e26689e9dfd4b0c6aaafb508343264c97
IgorFelipe10 <113717418+IgorFelipe10@users.noreply.github.com>
2024-04-03 22:38:26 -0300

    Update Produto.kt

--- app/src/main/java/com/example/delta/Produto.kt
+++ app/src/main/java/com/example/delta/Produto.kt
@@ -1,13 +1,14 @@
 package com.example.delta
 
+import com.google.gson.annotations.SerializedName
 
 data class Produto(
-    val PRODUTO_ID: Int,
-    val PRODUTO_NOME: String,
-    val PRODUTO_DESC: String,
-    val PRODUTO_PRECO: String,
-    val PRODUTO_DESCONTO: String,
-    val CATEGORIA_ID: Int,
-    val PRODUTO_ATIVO: Int
+    @SerializedName("PRODUTO_ID") val produtoId: Int,
+    @SerializedName("PRODUTO_NOME") val produtoNome: String,
+    @SerializedName("PRODUTO_DESC") val produtoDesc: String,
+    @SerializedName("PRODUTO_PRECO") val produtoPreco: String,
+    @SerializedName("PRODUTO_DESCONTO") val produtoDesconto: String,
+    @SerializedName("CATEGORIA_ID") val categoriaId: Int,
+    @SerializedName("PRODUTO_ATIVO") val produtoAtivo: Int,
+    @SerializedName("IMAGEM_URL") val imagemUrl: String?
 )
-
