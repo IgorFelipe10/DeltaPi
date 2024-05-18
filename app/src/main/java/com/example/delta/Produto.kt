da0967b9a816487ade9c9ce465ae26a7e3bacb95
Igor Felipe <igorfelipej@gmail.com>
2024-05-17 22:12:05 -0300

    Update Produto.kt

--- app/src/main/java/com/example/delta/Produto.kt
+++ app/src/main/java/com/example/delta/Produto.kt
@@ -1,18 +1,18 @@
 package com.example.delta
-
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Produto(
     @SerializedName("PRODUTO_ID") val produtoId: Int,
     @SerializedName("PRODUTO_NOME") val produtoNome: String,
-    @SerializedName("PRODUTO_DESC") val produtoDesc: String,
-    @SerializedName("PRODUTO_PRECO") val produtoPreco: String,  // Guardar como String privada
-    @SerializedName("PRODUTO_DESCONTO")  val produtoDesconto: String,  // Guardar como String privada
+    @SerializedName("PRODUTO_DESC") val produtoDesc: String?,
+    @SerializedName("PRODUTO_PRECO") val produtoPreco: Double?,
+    @SerializedName("PRODUTO_DESCONTO") val produtoDesconto: String?,
     @SerializedName("CATEGORIA_ID") val categoriaId: Int,
     @SerializedName("PRODUTO_ATIVO") val produtoAtivo: Int,
     @SerializedName("IMAGEM_URL") val imagemUrl: String?,
     @SerializedName("QUANTIDADE_DISPONIVEL") val quantidadeDisponivel: Int
-
 ) : Parcelable
 
